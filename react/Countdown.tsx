b5e734baac511c4331661f6dedda9130fa581a58
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2021-06-15 14:54:23 -0500

    [adcreative block feature

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {TimeSplit} from './typings/global'
+import {tick, getTwoDaysFromNow} from './utils/time'
 
+const DEFAULT_TARGET_DATE =  getTwoDaysFromNow()
 interface CountdownProps {
   targetDate: string
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate,}) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate=DEFAULT_TARGET_DATE}) => {
+  const [timeRemaining, setTime] = useState <TimeSplit> ({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+  tick(targetDate, setTime)
+  
   return (
     <h1> 
-      {targetDate} 
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`} 
     </h1>
   )
 }
